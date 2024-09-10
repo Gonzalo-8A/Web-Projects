daf661bb61cfc599b5151ba4fece4540d7e62525
Gonzalo Ochoa <gonzalo8amartin@gmail.com>
2024-09-10 14:39:01 +0200

    removeFromCart function

--- Amazon Project/scripts/checkout.js
+++ Amazon Project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurreny} from './utils/money.js';
 
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span data-product-id="${matchingProduct.id}" class="delete-quantity-link link-primary js-delete-link">
             Delete
           </span>
         </div>
@@ -96,3 +96,12 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const {productId} = link.dataset;
+      removeFromCart(productId);
+      console.log(cart);
+    })
+  })
