b6d998c68ce68f63abdc573df8f662c799b2a374
Gonzalo Ochoa <gonzalo8amartin@gmail.com>
2024-10-08 12:10:24 +0200

    Create Clothing Class

--- Amazon Project/data/products.js
+++ Amazon Project/data/products.js
@@ -34,23 +34,28 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-});
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+      Size Chart
+    </a>
+    `;
+  }
+}
 
 export const products = [
   {
@@ -278,6 +283,7 @@ export const products = [
       count: 160
     },
     priceCents: 1699,
+    type: "clothing",
     keywords: [
       "shorts",
       "apparel",
@@ -475,6 +481,7 @@ export const products = [
       count: 9017
     },
     priceCents: 2290,
+    type: "clothing",
     keywords: [
       "pants",
       "apparel",
@@ -552,6 +559,7 @@ export const products = [
       count: 248
     },
     priceCents: 2400,
+    type: "clothing",
     keywords: [
       "pants",
       "sweatpants",
@@ -704,6 +712,7 @@ export const products = [
       count: 3157
     },
     priceCents: 2400,
+    type: "clothing",
     keywords: [
       "sweaters",
       "hoodies",
@@ -712,6 +721,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails)
+  } 
   return new Product(productDetails);
 });
 
--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -36,6 +36,9 @@ products.forEach((product) => {
           <option value="10">10</option>
         </select>
       </div>
+
+      ${product.extraInfoHTML()}
+
       <div class="product-spacer"></div>
       <div class="added-to-cart">
         <img src="images/icons/checkmark.png">
