5f6951e89555da6b73e01c803fd45b3e41d14a61
Gonzalo Ochoa <gonzalo8amartin@gmail.com>
2024-11-13 12:32:40 +0100

    Solved all the Tests problems

--- Amazon Project/tests-jasmine/data/cartTest.js
+++ Amazon Project/tests-jasmine/data/cartTest.js
@@ -13,14 +13,15 @@ describe('test suite: addToCart', () => {
     });
     cart.loadFromStorage();
 
-    cart.addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart.length).toEqual(1);
+    cart.addToCartOnlyOne('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    
+    expect(cart.cartItems.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(2);
+    expect(cart.cartItems[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.cartItems[0].quantity).toEqual(2);
   });
 
-  it('adds a new product to the cart', () => {
+  it('adds a new product to the cart', async () => {
     spyOn(localStorage, 'setItem');
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
@@ -29,10 +30,11 @@ describe('test suite: addToCart', () => {
     cart.loadFromStorage();
 
     cart.addToCartOnlyOne('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart.length).toEqual(1);
+    
+    expect(cart.cartItems.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(1);
+    expect(cart.cartItems[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.cartItems[0].quantity).toEqual(1);
 
   });
 });
