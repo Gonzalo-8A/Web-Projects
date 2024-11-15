1c0287217d5273ccb3affa04be58af47b666d546
Gonzalo Ochoa <gonzalo8amartin@gmail.com>
2024-11-15 12:56:08 +0100

    Finish Amazon Search Bar

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -7,7 +7,27 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = '';
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search')
+  
+  let filteredProducts = products;
+
+  if(search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if(keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+
+      return matchingKeyword || 
+        product.name.toLowerCase().includes(search.toLowerCase)
+    });
+  }
+  
+  filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
@@ -94,4 +114,19 @@ function renderProductsGrid() {
         addedMessageTimeoutId = timeoutId;
       });
     });
+
+    document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    });
+  
+    document.querySelector('.js-search-bar')
+  .addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const search = document.querySelector('.js-search-bar').value;
+    window.location.href = `amazon.html?search=${search}`;}
+  });
 }
+
+  
