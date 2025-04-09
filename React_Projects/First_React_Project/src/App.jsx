63acd6b7565ab228aae60e769a0883a7e582de76
Gonzalo Ochoa <gonzalo8amartin@gmail.com>
2025-04-09 12:51:09 +0200

    update

--- React_Projects/First_React_Project/src/App.jsx
+++ React_Projects/First_React_Project/src/App.jsx
@@ -1,9 +1,6 @@
 // import { useState } from 'react'
 import reactLogo from "./assets/react-logo-xs.png";
-import blocksLogo from "./assets/blocks-logo.png";
-import mixLogo from "./assets/mix-logo.webp";
-import settingsIcon from "./assets/settings-icon.png";
-import stateLogo from "./assets/state-logo.png";
+import { CORE_CONCEPTS } from "./data.js";
 import "./App.css";
 
 const reactTitles = ["React Fundamentos", "React Bases", "React Principios"];
@@ -27,12 +24,12 @@ function Header() {
   );
 }
 
-function CoreConcepts(props) {
+function CoreConcepts({imagePath, title, description}) {
   return (
     <div className="coreConceptsCard">
-      <img src={props.src} alt="..." />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={imagePath} alt="..." />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
   );
 }
@@ -44,34 +41,17 @@ function App() {
       <section id="coreConcepts">
         <h2>Principales Características</h2>
         <div id="coreConceptsCards">
-          <CoreConcepts
-            src={blocksLogo}
-            title="Componentes"
-            description="El nucleo principal de construcción de una UI con React. La combinación de distintos componnentes logra una composición de una UI más manejable."
-          />
 
-          <CoreConcepts
-            src={mixLogo}
-            title="JSX"
-            description="Una combinación de código JS Y HTML. Retorna contenido HTML potencialmente dinámico que será renderizado para componer la UI."
-          />
+          <CoreConcepts {...CORE_CONCEPTS[0]}/>
+          <CoreConcepts {...CORE_CONCEPTS[1]}/>
+          <CoreConcepts {...CORE_CONCEPTS[2]}/>
+          <CoreConcepts {...CORE_CONCEPTS[3]}/>
 
-          <CoreConcepts
-            src={settingsIcon}
-            title="Props"
-            description="Proporcionan a los componentes la capacidad de poder tener atributos personalizables para que estos reciban información."
-          />
-
-          <CoreConcepts
-            src={stateLogo}
-            title="States"
-            description="Principales manejadores de datos en React, cuando encuentran un cambio en los datos obligan al componente a re-renderizarse y actualizar la interfaz."
-          />
         </div>
       </section>
-      <main>
+      {/* <main>
         <h2></h2>
-      </main>
+      </main> */}
     </div>
   );
 }
